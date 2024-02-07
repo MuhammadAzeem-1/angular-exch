cce01b7aa9c1896f1a88998b60cb80702f3f9851
Muhammad Azeem <mazeem182000@gmail.com>
2024-02-07 10:21:35 +0500

    updating navbar and adjucting the pixel

---
+++ src/app/gaming-section/lock-icon/lock-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lock-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './lock-icon.component.html',
+  styleUrl: './lock-icon.component.css'
+})
+export class LockIconComponent {
+
+}
--- src/app/shared/gaming-section/gaming-section.component.ts
+++ src/app/shared/gaming-section/gaming-section.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import LockIcon from '@mui/icons-material/Lock';
+
 
 @Component({
   selector: 'app-gaming-section',
@@ -11,7 +13,9 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
     CommonModule
   ],
   templateUrl: './gaming-section.component.html',
-  styleUrl: './gaming-section.component.css'
+  styleUrl: './gaming-section.component.css',
+  
+
 })
 export class GamingSectionComponent {
   isGameActive : boolean = true;
@@ -20,6 +24,7 @@ export class GamingSectionComponent {
   selectedPlayerIndex: number | null = null
 
   faXmark= faXmark;
+  LockIcon = LockIcon
 
 
    playerData:any = [
@@ -31,7 +36,7 @@ export class GamingSectionComponent {
     {
       name: "Player B",
       Back: 12,
-      Lay: 23,
+      Lay: 25,
     }
   ]
 
